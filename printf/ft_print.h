0d77824033d444209a199868b4597ac709ae0097
AlvaroPortatil <alvaroperezsaez7@hotmail.com>
2025-04-24 12:10:49 +0200

    empezamos

---
+++ printf/ft_print.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alvapere <<alvapere@student.42.fr>>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/24 11:22:41 by alvapere          #+#    #+#             */
+/*   Updated: 2025/04/24 12:02:53 by alvapere         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+# include <stdarg.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+int	ft_print_char(int character);
+
+#endif
